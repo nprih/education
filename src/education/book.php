f4f9f9f8b45c7d298722766c8f243dd621ad6646
Akchapocha <poiskraboti@inbox.ru>
2023-01-21 14:46:52 +0300

    Work

--- src/education/book.php
+++ src/education/book.php
@@ -2,33 +2,38 @@
 
 class ShopProduct
 {
-    public string $title;
-    public string $producerMainName;
-    public string $producerFirstName;
-    protected float $price;
-    public int $discount = 0;
+    protected int $discount = 0;
 
-    public function __construct(string $title, string $firstName, string $mainName, float $price)
+    public function __construct(private string $title,
+                                private string $producerFirstName,
+                                private string $producerMainName,
+                                protected int | float   $price)
     {
-        $this->title = $title;
-        $this->producerFirstName = $firstName;
-        $this->producerMainName = $mainName;
-        $this->price = $price;
     }
 
-    public function getProducer(): string
+    public function getProducerFirstName(): string
     {
-        return $this->producerFirstName . ' ' . $this->producerMainName;
+        return $this->producerFirstName;
     }
 
-    public function getSummaryLine(): string
+    public function getProducerMainName(): string
     {
-        return "{$this->title} ({$this->producerMainName}, {$this->producerFirstName})";
+        return $this->producerMainName;
     }
 
-    public function setDiscount(int $num): void
+    public function setDiscount(int|float $num): void
     {
         $this->discount = $num;
+    }
+
+    public function getDiscount(): int
+    {
+        return $this->discount;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
     }
 
     public function getPrice(): int|float
@@ -36,13 +41,22 @@ class ShopProduct
         return ($this->price - $this->discount);
     }
 
+    public function getProducer(): string
+    {
+        return $this->getProducerFirstName() . ' ' . $this->getProducerMainName();
+    }
+    public function getSummaryLine(): string
+    {
+        return "{$this->title} ({$this->producerMainName}, {$this->producerFirstName})";
+    }
+
 }
 
 class BookProduct extends ShopProduct
 {
-    public int $numPages;
-
-    public function __construct(string $title, string $firstName, string $mainName, float $price, int $numPages)
+    public function __construct(string $title, string $firstName,
+                                string $mainName, float $price,
+                                private int $numPages)
     {
         parent::__construct($title, $firstName, $mainName, $price);
         $this->numPages = $numPages;
@@ -55,7 +69,7 @@ class BookProduct extends ShopProduct
 
     public function getSummaryLine(): string
     {
-        return "{$this->title} ( $this->producerMainName, $this->producerFirstName ): {$this->numPages} стр.";
+        return parent::getSummaryLine() . ": - {$this->numPages} стр.";
     }
 
     public function getPrice(): int|float
@@ -67,21 +81,21 @@ class BookProduct extends ShopProduct
 
 class CDProduct extends ShopProduct
 {
-    public float $playLength;
-    public function __construct(string $title, string $firstName, string $mainName, float $price, float $playLength)
+    public function __construct(string $title, string $firstName,
+                                string $mainName, float $price,
+                                private float $playLength)
     {
         parent::__construct($title, $firstName, $mainName, $price);
-        $this->playLength = $playLength;
     }
 
-    public function getPlayLength(): string
+    public function getPlayLength(): int
     {
         return $this->playLength;
     }
 
     public function getSummaryLine(): string
     {
-        return "{$this->title} ( $this->producerMainName, $this->producerFirstName ): Время звучания {$this->playLength}";
+        return parent::getSummaryLine() . ": Время звучания {$this->playLength}";
     }
 
 }
@@ -92,4 +106,25 @@ $cdObj = new CDProduct('Классическая музыка', 'Антонио'
 debug($bookObj);
 debug($cdObj);
 
-debug(str_replace( $_SERVER['HOME'] . '/', '', __FILE__ ) . ' стр.: 94',1);
+class ShopProductWriter
+{
+    private array $products = [];
+
+    public function addProduct(ShopProduct $shopProduct): void
+    {
+        $this->products[] = $shopProduct;
+    }
+
+    public function write(): void
+    {
+        $str = '';
+        foreach ($this->products as $shopProduct) {
+            $str .= "{$shopProduct->title}";
+            $str .= $shopProduct->getProducer();
+            $str .= " ({$shopProduct->getPrice()})\n";
+        }
+        print $str;
+    }
+}
+
+debug(str_replace( $_SERVER['HOME'] . '/', '', __FILE__ ) . ' стр.: 106',1);
