e8d13fcc257020661642de98d01db6f9dbef0b10
Akchapocha <poiskraboti@inbox.ru>
2023-02-02 15:33:38 +0300

    Work

--- src/education/mySQL.php
+++ src/education/mySQL.php
@@ -183,16 +183,97 @@ function getSql(): array
     /** Решения задач из урока 1.6 */
 
     $sql['1.6'] = [
+        '1' => 'SELECT `name`, `city`, `per_diem`, `date_first`, `date_last` FROM `trip` 
+                 WHERE `name` LIKE \'%а %\' ORDER BY `date_last` DESC',
+
+        '2' => 'SELECT `name` FROM `trip` WHERE `city` = \'Москва\' GROUP BY `name` ORDER BY `name`',
+
+        '3' => 'SELECT `city`, count(`city`) AS `Количество` FROM `trip` GROUP BY `city` ORDER BY `city`',
+
+        '4' => 'SELECT `city`, count(`city`) AS `Количество` FROM `trip` GROUP BY `city` 
+                 ORDER BY count(`city`) DESC LIMIT 2',
+
+        '5' => 'SELECT `name`, `city`, DATEDIFF(`date_last`, `date_first`) + 1 AS `Длительность` 
+                FROM `trip` WHERE `city` NOT IN (\'Москва\', \'Санкт-Петербург\') ORDER BY `Длительность` DESC',
+
+        '6' => 'SELECT `name`, `city`, `date_first`, `date_last` FROM `trip` 
+                WHERE DATEDIFF(`date_last`, `date_first`) = (SELECT MIN(DATEDIFF(`date_last`, `date_first`)) AS `Dlit` 
+                                                             FROM `trip`)',
+
+        '7' => 'SELECT `name`, `city`, `date_first`, `date_last` FROM `trip` 
+                 WHERE MONTH(`date_first`) = MONTH(`date_last`) ORDER BY `city`, `name`',
+
+        '8' => 'SELECT MONTHNAME(`date_first`) AS `Месяц`, count(MONTHNAME(`date_first`)) AS `Количество` 
+                FROM `trip` GROUP BY `Месяц` ORDER BY `Количество` DESC, `Месяц`',
+
+        '9' => 'SELECT `name`, `city`, `date_first`, (DATEDIFF(`date_last`, `date_first`) + 1) * `per_diem` AS `Сумма` 
+                FROM `trip` 
+                WHERE YEAR(`date_first`) = \'2020\' AND MONTH(`date_first`) IN(2,3) ORDER BY `name`, `Сумма` DESC',
+
+        '10' => 'SELECT `name`, SUM((DATEDIFF(`date_last`, `date_first`) + 1) * `per_diem`) AS `Сумма` FROM `trip` 
+                 GROUP BY `name`
+                 HAVING `name` IN( SELECT `name` FROM `trip` GROUP BY `name` 
+                                                             HAVING COUNT(`name`) > 3) ORDER BY `Сумма` DESC',
+    ];
+
+    /** Решения задач из урока 1.7 */
+
+    $sql['1.7'] = [
+        '1' => 'CREATE TABLE `fine`(
+                    `fine_id` INT PRIMARY KEY AUTO_INCREMENT,
+                    `name` VARCHAR(30),
+                    `number_plate` VARCHAR(6),
+                    `violation` VARCHAR(50),
+                    `sum_fine` DECIMAL(8, 2),
+                    `date_violation` DATE,
+                    `date_payment` DATE
+                )',
+
+        '2' => 'INSERT INTO `fine` (`name`, `number_plate`, `violation`, `sum_fine`, `date_violation`, `date_payment`)
+                VALUES (\'Баранов П.Е.\', \'Р523ВТ\', \'Превышение скорости(от 40 до 60)\', NULL, \'2020-02-14\', NULL), 
+                        (\'Абрамова К.А.\', \'О111АВ\', \'Проезд на запрещающий сигнал\', NULL, \'2020-02-23\', NULL), 
+                        (\'Яковлев Г.Р.\', \'Т330ТТ\', \'Проезд на запрещающий сигнал\', NULL, \'2020-03-03\', NULL)',
+
+        '3' => 'UPDATE `fine` AS `f`, `traffic_violation` AS `tv`
+                SET `f`.`sum_fine` = `tv`.`sum_fine`
+                WHERE `f`.`violation` = `tv`.`violation` AND `f`.`sum_fine` IS NULL',
+
+        '4' => 'SELECT `name`, `number_plate`, `violation` FROM `fine` GROUP BY `name`, `number_plate`, `violation` 
+                HAVING COUNT(*) >= 2 ORDER BY `name`, `number_plate`, `violation`',
+
+        '5' => 'UPDATE `fine`, (SELECT `name`, `number_plate`, `violation` FROM `fine` 
+                                GROUP BY `name`, `number_plate`, `violation` HAVING COUNT(*) >= 2
+                                ORDER BY `name`, `number_plate`, `violation`) AS `query_in`
+                SET `fine`.`sum_fine` = `fine`.`sum_fine` * 2
+                WHERE `fine`.`date_payment` IS NULL
+                AND `fine`.`name` = `query_in`.`name` 
+                AND `fine`.`number_plate` = `query_in`.`number_plate` 
+                AND `fine`.`violation` = `query_in`.`violation`',
+
+        '6' => 'UPDATE `fine`, `payment`
+                SET `fine`.`date_payment` = `payment`.`date_payment`,
+                    `fine`.`sum_fine` = IF(DATEDIFF(`payment`.`date_payment`, `payment`.`date_violation`) <= 20, 
+                                            `fine`.`sum_fine` / 2, `fine`.`sum_fine`)
+                WHERE `fine`.`date_payment`IS NULL 
+                AND `fine`.`name` = `payment`.`name`
+                AND `fine`.`number_plate` = `payment`.`number_plate` 
+                AND `fine`.`violation` = `payment`.`violation` 
+                AND `fine`.`date_violation` = `payment`.`date_violation`;',
+
+        '7' => 'CREATE TABLE `back_payment` (SELECT `name`, `number_plate`, `violation`, `sum_fine`, `date_violation` 
+                                            FROM `fine` WHERE `date_payment` IS NULL)',
+
+        '8' => 'DELETE FROM `fine` WHERE DATEDIFF(`date_violation`, \'2020-02-01\') < 0',
+    ];
+
+    /** Решения задач из урока 2.1 */
+
+    $sql['2.1'] = [
         '1' => '',
         '2' => '',
         '3' => '',
         '4' => '',
         '5' => '',
-        '6' => '',
-        '7' => '',
-        '8' => '',
-        '9' => '',
-        '10' => '',
     ];
 
     return $sql;
