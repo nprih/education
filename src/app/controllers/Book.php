ed8efa7a731b61de7b905a053ec9e880598765ad
Akchapocha <poiskraboti@inbox.ru>
2023-02-15 14:44:13 +0300

    Work

--- src/app/controllers/Book.php
+++ src/app/controllers/Book.php
@@ -2,23 +2,15 @@
 
 namespace app\controllers;
 
-use education\classes\Controller;
-use education\classes\TileForces;
-use education\classes\UnitAcquisition;
-
-
 class Book
 {
     public function indexAction():void
     {
 
-        $acquirer = new UnitAcquisition();
-        $tileforces = new TileForces(4, 2, $acquirer);
-        $power = $tileforces->firepower();
-        debug("Огневая мощь: {$power}<br>");
+
 
         debug('</br></br>');
         debug('Класс: ' . __CLASS__ . '</br>Метод: ' . __FUNCTION__);
-        debug(str_replace( $_SERVER['HOME'] . '/', '', __FILE__ ) . ' стр.: 452',1);
+        debug(str_replace( $_SERVER['HOME'] . '/', '', __FILE__ ) . ' стр.: 525',1);
     }
 }
