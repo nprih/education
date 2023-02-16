c74cccb1587fc5cfc63b4fb316451391b2e6ace0
nprih <pulong@list.ru>
2023-02-16 09:52:54 +0300

    Home

--- src/education/mySQL.php
+++ src/education/mySQL.php
@@ -904,7 +904,34 @@ function getSql(): array
                 HAVING COUNT(`step_name`) = 2
                 ORDER BY `Шаг`',
 
-        '4' => '',
+        '4' => 'SELECT  
+                    CASE
+                        WHEN `rate` <= 10 THEN \'I\'
+                        WHEN `rate` <= 15 THEN \'II\'
+                        WHEN `rate` <= 27 THEN \'III\'
+                        ELSE \'IV\'
+                    END AS `Группа`, 
+                    CASE
+                        WHEN `rate` <= 10 THEN \'от 0 до 10\'
+                        WHEN `rate` <= 15 THEN \'от 11 до 15\'
+                        WHEN `rate` <= 27 THEN \'от 16 до 27\'
+                        ELSE \'больше 27\'
+                    END AS `Интервал`, COUNT(`student_id`) AS `Количество`
+                FROM      
+                    (
+                     SELECT `student_id`, COUNT(*) AS `rate`
+                     FROM 
+                         (
+                          SELECT `student_id`, `step_id`
+                          FROM `step_student`
+                          WHERE `result` = \'correct\'
+                          GROUP BY `student_id`, `step_id`
+                         ) `query_in`
+                     GROUP BY `student_id`
+                     ORDER BY 2
+                    ) `query_in_1`
+                GROUP BY `Группа`, `Интервал`',
+
         '5' => '',
         '6' => '',
         '7' => '',
