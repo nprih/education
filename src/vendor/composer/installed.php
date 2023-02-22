66e533dba529ca5938d2a85d904bc568a003b756
Akchapocha <poiskraboti@inbox.ru>
2023-02-22 14:50:47 +0300

    Work Приложение работает на рукописном Router используя автозагрузку классов из Composer

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9b69e4181d01913211e780fbc47ed6640822893c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9b69e4181d01913211e780fbc47ed6640822893c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
