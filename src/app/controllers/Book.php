424c2cb7bc4b2f39899678b6ef036234649caf12
Akchapocha <poiskraboti@inbox.ru>
2023-03-03 10:38:59 +0300

    Work

--- src/app/controllers/Book.php
+++ src/app/controllers/Book.php
@@ -20,6 +20,6 @@ class Book
 
         debug('</br></br>');
         debug('Класс: ' . __CLASS__ . '</br>Метод: ' . __FUNCTION__);
-        debug(str_replace( $_SERVER['HOME'] . '/', '', __FILE__ ) . ' стр.: 810',1);
+        debug(str_replace( $_SERVER['HOME'] . '/', '', __FILE__ ) . ' стр.: 868',1);
     }
 }
