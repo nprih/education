8590561ba0a63a82e6322a6735d72ad8c090156f
Николай Приходько <akchapocha@gmail.com>
2023-03-08 13:46:23 +0300

    Work

--- src/app/controllers/Book.php
+++ src/app/controllers/Book.php
@@ -9,17 +9,21 @@ class Book
 {
     public function indexAction():void
     {
-        $store = new UserStore();
-        $store->addUser('bob wiliams', 'bob@example.com', '12345');
-        $validator = new Validator($store);
 
-        if ($validator->validateUser('bob@example.com', '12345'))
-        {
-            debug('Привет, друг!<br>');
-        }
 
         debug('</br></br>');
         debug('Класс: ' . __CLASS__ . '</br>Метод: ' . __FUNCTION__);
-        debug(str_replace( $_SERVER['HOME'] . '/', '', __FILE__ ) . ' стр.: 125, Slim, PHPUnit, Twig',1);
+        debug(str_replace( $_SERVER['HOME'] . '/', '', __FILE__ ) . ' стр.: 135, Slim, PHPUnit, Twig');
+
+        echo '<style>body{font-size: 16px;}</style>
+                src:<br>
+                https://www.pascallandau.com/blog/run-laravel-9-docker-in-2022/<br>
+                https://git.pleer.ru/prinik-group<br>
+                https://buildmedia.readthedocs.org/media/pdf/phpunit-documentation-russian/latest/phpunit-documentation-russian.pdf
+                ';
+
+
+        debug('STOP',1);
+
     }
 }
