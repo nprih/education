5410127aa111165d186ba7a1c6d1244c6dd6fa0a
Николай Приходько <akchapocha@gmail.com>
2023-03-14 11:47:32 +0300

    Work

--- src/app/controllers/Book.php
+++ src/app/controllers/Book.php
@@ -9,14 +9,19 @@ class Book
     {
         debug('</br></br>');
         debug('Класс: ' . __CLASS__ . '</br>Метод: ' . __FUNCTION__);
-        debug(str_replace($_SERVER['HOME'] . '/', '', __FILE__) . ' стр.: 168, Slim, PHPUnit, Twig');
+        debug(str_replace($_SERVER['HOME'] . '/', '', __FILE__) . ' стр.: 185, Slim, PHPUnit, Twig');
 
-        echo '<style>body{font-size: 16px;}</style>
-                src:<br>
-                https://www.pascallandau.com/blog/run-laravel-9-docker-in-2022/<br>
-                https://git.pleer.ru/prinik-group<br>
-                https://buildmedia.readthedocs.org/media/pdf/phpunit-documentation-russian/latest/phpunit-documentation-russian.pdf
-                ';
+        echo
+<<<'EOF'
+<style>body{font-size: 16px;}</style>
+src:<br>
+https://www.pascallandau.com/blog/run-laravel-9-docker-in-2022/<br>
+https://git.pleer.ru/prinik-group<br>
+https://buildmedia.readthedocs.org/media/pdf/phpunit-documentation-russian/latest/phpunit-documentation-russian.pdf<br><br>
+https://php-di.org/doc/<br>
+https://www.slimframework.com/docs/v4/concepts/di.html<br><br>
+https://www.doctrine-project.org/projects.html<br>                
+EOF;
 
         debug('STOP', 1);
     }
