f53822aee4a2276428a8cd852b2a28dc6d660147
Николай Приходько <akchapocha@gmail.com>
2023-04-04 14:35:53 +0300

    Work

--- src/app/controllers/Book.php
+++ src/app/controllers/Book.php
@@ -9,7 +9,7 @@ class Book
     {
         debug('</br></br>');
         debug('Класс: ' . __CLASS__ . '</br>Метод: ' . __FUNCTION__);
-        debug(str_replace($_SERVER['HOME'] . '/', '', __FILE__) . ' стр.: 114, Slim, PHPUnit, Twig');
+        debug(str_replace($_SERVER['HOME'] . '/', '', __FILE__) . ' стр.: 143, Slim, PHPUnit, Twig');
 
         echo
 <<<'EOF'
