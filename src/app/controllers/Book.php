0041fba62238f98997636e06faa105ceb8fafbb5
Николай Приходько <akchapocha@gmail.com>
2023-04-06 12:53:35 +0300

    Work

--- src/app/controllers/Book.php
+++ src/app/controllers/Book.php
@@ -7,7 +7,6 @@ class Book
 {
     public function indexAction(): void
     {
-        $obj = new \stdClass();
         debug('</br></br>');
         debug('Класс: ' . __CLASS__ . '</br>Метод: ' . __FUNCTION__);
         debug(str_replace($_SERVER['HOME'] . '/', '', __FILE__) . ' стр.: 190, Slim, PHPUnit, Twig');
